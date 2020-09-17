7de1a34452375ef33b13d17c662a279dd46be9ec
Dmytro Verkhovsky <36972220+Dovahkiin169@users.noreply.github.com>
2020-09-17 20:23:29 +0200

    login validator

--- bankApp/src/bankPackage/loginActivity.java
+++ bankApp/src/bankPackage/loginActivity.java
@@ -3,6 +3,8 @@ package bankPackage;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class loginActivity extends JFrame implements ActionListener {
 
@@ -11,6 +13,7 @@ public class loginActivity extends JFrame implements ActionListener {
     public JPasswordField fieldPassword;
     public JButton buttonLogin;
     public JCheckBox chinButton;
+    EmailValidator EV = new EmailValidator();
 
     public loginActivity()  {
         super("BankApp");
@@ -21,7 +24,7 @@ public class loginActivity extends JFrame implements ActionListener {
         fieldPassword = new JPasswordField(20);
         buttonLogin = new JButton("Login");
         chinButton = new JCheckBox("as Employee");
-        
+
         chinButton.setMnemonic(KeyEvent.VK_C);
         chinButton.setSelected(true);
 
@@ -76,9 +79,43 @@ public class loginActivity extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         Object source = e.getSource();
-        if (source == buttonLogin && chinButton.isSelected())
+
+        if (source == buttonLogin && chinButton.isSelected() && canLogin())
             System.out.println("Login Employee");
-        else if(source == buttonLogin && !chinButton.isSelected())
+        else if(source == buttonLogin && !chinButton.isSelected() && canLogin())
             System.out.println("Login User");
+    }
+
+    public boolean canLogin() {
+        if (EV.validate(textUsername.getText()))
+            if (fieldPassword.getPassword().length != 0)
+                return true;
+            else
+                return false;
+        else
+            return false;
+    }
+}
+
+
+
+class EmailValidator {
+
+    private Pattern pattern;
+    private Matcher matcher;
+
+    private static final String EMAIL_PATTERN =
+            "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                    + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+    public EmailValidator() {
+        pattern = Pattern.compile(EMAIL_PATTERN);
+    }
+
+    public boolean validate(final String hex) {
+
+        matcher = pattern.matcher(hex);
+        return matcher.matches();
+
     }
 }
